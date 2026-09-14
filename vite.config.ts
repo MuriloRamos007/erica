9a655da0823d0675dd8a22e5926b6b9b40eb7f26
Murilo Ramos <151067196+MuriloRamos007@users.noreply.github.com>
2026-09-14 10:03:04 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { existsSync } from "node:fs";
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: '/aniversario-erica/',
   define: {
     "import.meta.env.VITE_HAS_MUSIC": JSON.stringify(
       existsSync("public/music.mp3"),
